a723835d194d9bd744ccb0b1479e6e967b516b22
Eren Ertem <mail.erenertem@gmail.com>
2025-05-24 18:10:32 +0300

    add sol1

---
+++ Sol1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Sol1 {
+
+  public static void main(String[] args) {
+    System.out.println("hi");
+  }
+}
--- Solution.java
+++ Solution.java
@@ -7,19 +7,6 @@ class Departmen{
 
 public class Solution{
     public static void main(String[] args){
-
-        Integer deptNumber;
-        Integer empNumber;
-
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("enter the number of departments: ");
-        deptNumber = scan.nextInt();
-
-        System.out.println("enter the number of the eployees: ");
-        empNumber = scan.nextInt();
-
-
-
+        
     }
 }
