cc9f04d34d3789a3044a2ee62b551cfe07193bd1
Eren Ertem <mail.erenertem@gmail.com>
2025-05-24 18:05:13 +0300

    first commit

---
+++ Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Departmen{
+    private Integer id;
+    
+}
+
+public class Solution{
+    public static void main(String[] args){
+
+        Integer deptNumber;
+        Integer empNumber;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter the number of departments: ");
+        deptNumber = scan.nextInt();
+
+        System.out.println("enter the number of the eployees: ");
+        empNumber = scan.nextInt();
+
+
+
+    }
+}
