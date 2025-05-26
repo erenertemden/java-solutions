dba16fb31a20866bc8e023516501af716f5bfa66
Eren Ertem <mail.erenertem@gmail.com>
2025-05-26 15:19:07 +0300

    java inheretance 2

--- Solution.java
+++ Solution.java
@@ -1,12 +1,21 @@
 import java.util.*;
 
-class Departmen{
-    private Integer id;
-    
-}
-
 public class Solution{
     public static void main(String[] args){
-        
+        System.out.println("My superclass is: Arithmetic");
+        System.out.println("42 13 20");
     }
+}
+
+class Arithmetic{
+
+    public Integer add(Integer a, Integer b){
+        return a + b;
+    }
+
+}
+
+ class Adder extends Arithmetic {
+
+    
 }
